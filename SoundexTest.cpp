3f824f20492d24c753f34e8e878a3b40f048ec6e
Simon Obute <simon.obute@arm.com>
2021-03-26 12:39:58 +0000

    zero-pad one letter word with 3 0s

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,8 +1,16 @@
 #include "gmock/gmock.h"
 #include "soundex.hpp"
+class SoundexEncoding : public testing::Test {
+    public:
+        Soundex soundex;
+};
 
-TEST(SoundexEncoding, RetainFirstLetterOfWord) {
-    Soundex soundex;
-    std::string encoded_word = soundex.encode("A");
+TEST_F(SoundexEncoding, RetainFirstLetterOfWord) {
+    std::string encoded_word = soundex.encode("Abcde");
     ASSERT_EQ(encoded_word.front(),'A');
+}
+
+TEST_F(SoundexEncoding, ZeroPadOneLetterWord) {
+    std::string encoded_word = soundex.encode("B");
+    ASSERT_EQ(encoded_word,"B000");
 }
--- soundex.cpp
+++ soundex.cpp
@@ -1,4 +1,8 @@
 #include "soundex.hpp"
 std::string Soundex::encode(std::string word) {
+    if(word.length() == 1)
+    {
+        return word + "000";
+    }
     return word;
 }
